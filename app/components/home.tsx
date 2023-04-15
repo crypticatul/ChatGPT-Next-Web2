cc774c43e3fb636c59f018108961b165a6527121
crypticatul <work.atulsingh@gmail.com>
2023-04-16 03:12:10 +0530

    Logo another attempt
    Logo

--- app/components/home.tsx
+++ app/components/home.tsx
@@ -207,7 +207,7 @@ function _Home() {
               />
             </div>
             <div className={styles["sidebar-action"]}>
-              <a href={REPO_URL} target="_blank">
+             <a href={https://cognitiev.com/</div>} target="_blank">
                 <IconButton icon={<GithubIcon />} shadow />
               </a>
             </div>
